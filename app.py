639e1b8fd5c7e779a5c655cd81063c32785a2bab
pdhoward <patrick.howard@hotmail.com>
2023-02-12 15:20:37 -0600

    first query test

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ import os
 import json
 import flask
 import openai
+import pinecone
 
 from dotenv import load_dotenv
 
-
-
 load_dotenv()
 
-WEAVIATE_URL = os.environ.get("WEAVIATE_URL")
 OPEN_API_KEY = os.environ.get("OPENAI_API_KEY")
+PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
+PINECONE_ENV = os.environ.get("PINECONE_ENV")
 
 #######################################
 # get API key from top-right dropdown on OpenAI website
@@ -20,7 +20,26 @@ openai.api_key = OPEN_API_KEY
 app = flask.Flask(__name__)
 port = int(os.getenv("PORT", 9099))
 
-query = "who was the 12th person on the moon and when did they land?"
+embed_model = "text-embedding-ada-002"
+
+index_name = 'product'
+
+# initialize connection to pinecone (get API key at app.pinecone.io)
+pinecone.init(
+    api_key=PINECONE_API_KEY,
+    environment=PINECONE_ENV
+)
+
+# connect to index
+index = pinecone.Index(index_name)
+# view index stats
+print(index.describe_index_stats())
+
+###############################################
+######   openai query before vectors     #####
+##############################################
+
+query = "what is the lowest priced hammer that is available?"
 
 # function to handle openai prompt
 def complete(prompt):
@@ -37,8 +56,73 @@ def complete(prompt):
     )
     return res['choices'][0]['text'].strip()
 
+result = complete(query)
+print(result)
+answer_returned = 'The lowest priced hammer available is the Stanley FatMax Xtreme AntiVibe Rip Claw Hammer, which retails for around $10'
+
+###############################################
+######   openai query with vectors       #####
+##############################################
+
+limit = 3750
+
+def retrieve(query):
+    res = openai.Embedding.create(
+        input=[query],
+        engine=embed_model
+    )
+
+    # retrieve from Pinecone
+    xq = res['data'][0]['embedding']
+
+    # get relevant contexts
+    res = index.query(xq, top_k=3, include_metadata=True)
+    print(res)
+    contexts = [
+        x['metadata']['description'] for x in res['matches']        
+    ]
+    #contexts.append(x['metadata']['name'] for x in res['matches'])
+    #contexts.append(x['metadata']['brand'] for x in res['matches'])   
+    #contexts.append(x['metadata']['specifications'] for x in res['matches'])
+
+    # build our prompt with the retrieved contexts included
+    prompt_start = (
+        "Answer the question based on the context below.\n\n"+
+        "Context:\n"
+    )
+    prompt_end = (
+        f"\n\nQuestion: {query}\nAnswer:"
+    )
+    # append contexts until hitting limit
+    for i in range(1, len(contexts)):
+        if len("\n\n---\n\n".join(contexts[:i])) >= limit:
+            prompt = (
+                prompt_start +
+                "\n\n---\n\n".join(contexts[:i-1]) +
+                prompt_end
+            )
+            break
+        elif i == len(contexts)-1:
+            prompt = (
+                prompt_start +
+                "\n\n---\n\n".join(contexts) +
+                prompt_end
+            )
+    return prompt
+
+# using same query prompt - we first retrieve relevant contexts
+query_with_contexts = retrieve(query)
+
+# then we complete the context-infused query
+vector_result = complete(query_with_contexts)
+print(vector_result)
+
+vector_answer='Ivy Classic SDS plus tungsten carbide hammer bits are the lowest priced hammer available.'
 
 
+
+
+"""
 @app.route('/message', methods=['POST'])
 def message():
     query = "What is an agent"    
@@ -48,3 +132,5 @@ def message():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
+"""
+
